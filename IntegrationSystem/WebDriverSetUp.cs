3d9ec69233c9cbcce107f72eedd2e7bb6e2fb189
Trần Thiện Phú <hostcode0301@gmail.com>
2021-07-07 19:15:33 +0700

    phutt: Add selenium + Update user info test

---
+++ IntegrationSystem/WebDriverSetUp.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace AngeloidTest.IntegrationSystem
+{
+    public class WebDriverSetUp
+    {
+        protected IWebDriver webDriver;
+        protected IJavaScriptExecutor js;
+
+        //Set up web driver
+        public void Setup(String browserName)
+        {
+            switch (browserName) {
+                case "chrome":
+                    //set up chrome web driver
+                    webDriver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    //set up firefox web driver
+                    webDriver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new Exception("Does not support web browser");
+            }
+
+            //JavaScript setup
+            js = (IJavaScriptExecutor)webDriver;
+
+            //Full screen mode
+            webDriver.Manage().Window.Maximize();
+
+            //Every line waiting for appear object in 10s
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            //Close web driver object
+            webDriver.Quit();
+        }
+
+        //Test browser list
+        public static IEnumerable<string> BrowserToRunWith() {
+            string[] browsers = {
+                "chrome", "firefox"
+            };
+
+            foreach (string item in browsers)
+            {
+                yield return item;
+            }
+        }
+    }
+}
